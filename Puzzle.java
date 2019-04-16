9afd8ac333872d1b727195889f13bab44747bd60
YasQueen <k.ozdemir@student.uliege.be>
2019-04-16 14:58:08 +0200

    code won't work. thinking of an idea to find the solutions brb queen

--- Puzzle.java
+++ Puzzle.java
@@ -1,9 +1,19 @@
 
 public class Puzzle{
    private final int position;
+   private final boolean isHexagone;
    Shape shape;
    Facet facets;
-   Puzzle neighbor;//nécessaire ?
+
+   public Puzzle(int i, int[] connections){
+      position = i;
+      facets = Facet(connections);
+      shape = null;
+      if(length(connections) == 6)
+         isHexagone = true;
+      else
+         isHexagone = false;
+   }
 
    public boolean threatens(Shape newShape){
       //à voir pour l'input de isCompatible
@@ -12,7 +22,7 @@ public class Puzzle{
          this.shape = newShape;
          return true;
       }
-      if(newShape.rotate())
+      if(newShape.rotate() == true)
          return threatens(newShape);//newShape a été rotationné
 
       return false;
--- SoccerBall.java
+++ SoccerBall.java
@@ -1,5 +1,14 @@
 
 public class SoccerBall{
+   Data data;
+   int[][] elementHexagone;
+   int[] nbElementsHexagone;
+   int[][] elementPentagone;
+   int[] nbElementsPentagone;
+   private Node listHexagone;
+   private Node listPentagone;
+   private Puzzle[] puzzle;
+   Shape shape;
 
    private Node setStock(int[][] elements, int[] nbElements){
       Shape shape = null;
@@ -16,6 +25,44 @@ public class SoccerBall{
       return list;
    }
 
+   private void findSolution(int position){
+      if(position == 32)
+         return;
+      Shape newShape = findShape(puzzle.isHexagone);
+      if(newShape == null){
+         puzzle.shape.isUsed = false;
+         puzzle.shape.rotate();
+         puzzle.facets.clearConnection(puzzle.shape.concavity);
+         puzzle.shape = null;
+         findSolution( position-1);
+      }
+      if(puzzle.threatens() == true)
+         findSolution( position+1);
+      else
 
+   }
+
+   public void main(){
+
+      for (int i = 0; i < 14; i++) {
+         if(i < 10){
+         elementHexagone[i] = data.ELEMENTS_SIDES[i];
+         nbElementsHexagone[i] = data.NB_ELEMENTS[i];
+         }
+         else{
+            elementPentagone[i] = data.ELEMENTS_SIDES[i];
+            nbElementsPentagone[i] = data.NB_ELEMENTS[i];
+         }
+      }
+
+      listHexagone = setStock(elementHexagone,nbElementsHexagone);
+      listPentagone = setStock(elementPentagone,nbElementsPentagone);
+
+      for (int i = 1; i <= 32; i++) {
+         puzzle[i] = new Puzzle(i, data.CONNECTIONS(i));
+      }
+
+      
+   }
 
 }
